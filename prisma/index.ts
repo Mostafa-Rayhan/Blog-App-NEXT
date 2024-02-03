b5f1294177ae0d8a009330c26a9575beafbc7e14
Mostafa Rayhan <mostafarayhan57@gmail.com>
2024-02-03 22:01:11 +0600

    code push

---
+++ prisma/index.ts
@@ -0,0 +1,32 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: PrismaClient;
+    }
+  }
+}
+
+
+
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.prisma) {
+//     global.prisma = new PrismaClient();
+//   }
+//   prisma = global.prisma;
+// }
+
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
+
+export default prisma;
